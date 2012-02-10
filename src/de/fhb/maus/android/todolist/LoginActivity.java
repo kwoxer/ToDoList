f6226b7806cc647feda5d53267ea5f0aa579f9a7
kwoxer <kwoxer@kwoxer-lappy>
2012-02-10 18:08:57 +0100

    Grundlagen zum nutzen der Login Funktionalität

--- src/de/fhb/maus/android/todolist/LoginActivity.java
+++ src/de/fhb/maus/android/todolist/LoginActivity.java
@@ -1,5 +1,10 @@
 package de.fhb.maus.android.todolist;
 
+import java.util.ArrayList;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import de.fhb.maus.android.todolist.database.CustomHttpClient;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +12,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends Activity {
@@ -14,6 +20,7 @@ public class LoginActivity extends Activity {
 
 	private Button mLogIn;
 	private boolean toastAlreadyShown = false;
+	private TextView error;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,20 +37,71 @@ public class LoginActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				EditText emailField = (EditText) findViewById(R.id.editTextEmail);
-				String email = emailField.getText().toString();
-				if (email.length() == 0) {
-					if (!toastAlreadyShown) {
-						Toast toast = Toast.makeText(getApplicationContext(),
-								getResources().getString(R.string.login_toast),
-								Toast.LENGTH_SHORT);
-						toast.show();
-						toastAlreadyShown = true;
-						return;
-					}
-				} else
-					// TODO Login with server and database here
-					startActivity(new Intent(LoginActivity.this,
-							TodoListActivity.class));
+				EditText pwField = (EditText) findViewById(R.id.editTextPassword);
+
+				String username = emailField.getText().toString();
+				String password = pwField.getText().toString();
+
+				ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+				postParameters.add(new BasicNameValuePair("mail", username));
+				postParameters.add(new BasicNameValuePair("pwhash", password));
+
+				String response = null;
+				try {
+					response = CustomHttpClient.executeHttpPost(
+							"http://10.0.2.2:8080/login.php?", postParameters);
+
+					String res = response.toString();
+					res = res.replaceAll("\\s+", "");
+					if (res.equals("1"))
+						error.setText("Correct Username or Password");
+					else
+						error.setText("Sorry!! Incorrect Username or Password");
+				} catch (Exception e) {
+					emailField.setText(e.toString());
+				}
+
+				//
+				// try {
+				// if (username.length() > 0 && password.length() > 0) {
+				// DBUserAdapter dbUser = new DBUserAdapter(
+				// LoginActivity.this);
+				// dbUser.open();
+				// //dbUser.AddUser(username, password);
+				//
+				// if (dbUser.Login(username, password)) {
+				// Toast.makeText(LoginActivity.this,
+				// "Successfully Logged In", Toast.LENGTH_LONG)
+				// .show();
+				// startActivity(new Intent(LoginActivity.this,
+				// TodoListActivity.class));
+				// } else {
+				// Toast.makeText(LoginActivity.this,
+				// "Invalid Username/Password",
+				// Toast.LENGTH_LONG).show();
+				// }
+				// dbUser.close();
+				// }
+				//
+				// } catch (Exception e) {
+				// Toast.makeText(LoginActivity.this, e.getMessage(),
+				// Toast.LENGTH_LONG).show();
+				// }
+
+				// String email = emailField.getText().toString();
+				// if (email.length() == 0) {
+				// if (!toastAlreadyShown) {
+				// Toast toast = Toast.makeText(getApplicationContext(),
+				// getResources().getString(R.string.login_toast),
+				// Toast.LENGTH_SHORT);
+				// toast.show();
+				// toastAlreadyShown = true;
+				// return;
+				// }
+				// } else
+				// // TODO Login with server and database here
+				// startActivity(new Intent(LoginActivity.this,
+				// TodoListActivity.class));
 			}
 		});
 	}
