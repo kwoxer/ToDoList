cc17bf93e03700ab79850abd51892c607ec267bd
kwoxer <kwoxer@kwoxer-lappy>
2012-02-11 11:13:09 +0100

    new login

--- php/login.php
+++ php/login.php
@@ -4,16 +4,18 @@ $username = 'root';
 $pws = 'test';
 
 try {
-    $name = $_GET['name'];
-    $pw = $_GET['pw'];
+    $name = $_POST['name'];
+    $pw = $_POST['pw'];
 
     $dbh = new PDO("mysql:host=$hostname;dbname=toDoList", $username, $pws);
 
     $stmt = $dbh->prepare("SELECT * FROM account WHERE name = '$name'");
     $stmt->execute();
-
+	
+	
     $result = $stmt->fetchAll();
-    
+   
+	
     foreach($result as $row)
     	{
         	echo ($pw == $row['password'] ? '1' : '0');
