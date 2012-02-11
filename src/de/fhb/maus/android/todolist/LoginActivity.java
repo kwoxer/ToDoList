b9afde3cd51181fd80fcc43c2845961d06daeef0
kwoxer <kwoxer@kwoxer-lappy>
2012-02-11 16:32:31 +0100

    Login endlich fertig

--- src/de/fhb/maus/android/todolist/LoginActivity.java
+++ src/de/fhb/maus/android/todolist/LoginActivity.java
@@ -1,10 +1,10 @@
 package de.fhb.maus.android.todolist;
 
 import java.util.ArrayList;
-
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import de.fhb.maus.android.todolist.database.CustomHttpClient;
+import de.fhb.maus.android.todolist.validator.EmailValidator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,10 +19,11 @@ import android.widget.Toast;
 public class LoginActivity extends Activity {
 	/** Called when the activity is first created. */
 
-	private Button mLogIn;
+	private Button mLogIn, mExit;
 	private boolean toastAlreadyShown = false;
 	private EditText emailField, pwField;
 	private TextView error;
+	private EmailValidator ev;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,40 +33,27 @@ public class LoginActivity extends Activity {
 		pwField = (EditText) findViewById(R.id.editTextPassword);
 		mLogIn = (Button) findViewById(R.id.buttonLogin);
 		error = (TextView) findViewById(R.id.textViewError);
+		mExit = (Button) findViewById(R.id.buttonExit);
 
-		
-		Button exit = (Button)findViewById(R.id.buttonExit);
-		exit.setOnClickListener(new OnClickListener() {
-			
+		mExit.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				startActivity(new Intent(LoginActivity.this,TodoListActivity.class));
-				
+				startActivity(new Intent(LoginActivity.this,
+						TodoListActivity.class));
 			}
 		});
-		
-		
+
 		mLogIn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				String username = emailField.getText().toString();
 				String password = pwField.getText().toString();
-
-				if (username.length() == 0) {
-					if (!toastAlreadyShown) {
-						Toast toast = Toast.makeText(getApplicationContext(),
-								getResources().getString(R.string.login_toast),
-								Toast.LENGTH_SHORT);
-						toast.show();
-						toastAlreadyShown = true;
-						return;
-					}
-				} else {
+				ev = new EmailValidator();
+				if (ev.validate(username)) {
 					ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
 					postParameters
 							.add(new BasicNameValuePair("name", username));
 					postParameters.add(new BasicNameValuePair("pw", password));
-
 					String response = null;
 					try {
 						response = CustomHttpClient.executeHttpPost(
@@ -73,15 +61,24 @@ public class LoginActivity extends Activity {
 								postParameters);
 						String res = response.toString();
 						res = res.replaceAll("\\s+", "");
-						if (res.equals("1"))
+						if (res.equals("1")) {
+							error.setText("Login accepted");
 							startActivity(new Intent(LoginActivity.this,
 									TodoListActivity.class));
-						else
-							error.setText("0");
+						} else {
+							error.setText("Login not accepted");
+						}
 					} catch (Exception e) {
 						Log.e("Database", e.toString());
 					}
-
+				} else {
+					if (!toastAlreadyShown) {
+						Toast toast = Toast.makeText(getApplicationContext(),
+								getResources().getString(R.string.login_toast),
+								Toast.LENGTH_SHORT);
+						toast.show();
+						toastAlreadyShown = true;
+					}
 				}
 			}
 		});
